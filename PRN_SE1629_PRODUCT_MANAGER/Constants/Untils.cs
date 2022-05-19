7375a2a1afc5c75b08592231aafc844c4503df38
DONGBHPC\Dong Bui Huu <buihuudong19@gmail.com>
2022-05-19 10:41:36 +0700

    commit

---
+++ PRN_SE1629_PRODUCT_MANAGER/Constants/Untils.cs
@@ -0,0 +1,7 @@
+﻿namespace Prn.Se1629;
+/*Static class come here*/
+public static class Utils
+{
+    public static int InitLengProduct = 1;
+    public static int Max(int a, int b) => Math.Max(a, b);//Expression-Bodied
+}
--- PRN_SE1629_PRODUCT_MANAGER/Program.cs
+++ PRN_SE1629_PRODUCT_MANAGER/Program.cs
@@ -1,1 +1,19 @@
 ﻿namespace Prn.Se1629;
+using static System.Console;
+public class Program
+{
+    public static void Main()
+    {
+        /*1. Khai bao doi tuong ManagerProduct*/
+        ProductManagement productManagement = new ProductManagement();
+        /*2. Them n product vao*/
+        productManagement.Add(new Product(1, "Laptop Dell 15in", "new product with new features 2022", 2500, 2));
+        productManagement.Add(new Product(2, "Macbook Pro 2022", "new product with new features 2022", 500, 3));
+        productManagement.Add(new Product(3, "iPhone 14 Pro", "new product with new features 2022", 2500, 4));
+        productManagement.Add(new Product(4, "Samsung galay note 31", "new product with new features 2022", 1360, 5));
+
+        /*3. call display all products*/
+        productManagement.Display();
+
+    }
+}
