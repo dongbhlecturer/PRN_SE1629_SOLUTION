239d26c6fe08a5c0edf58c3b1a10adc54db74a0f
DONGBHPC\Dong Bui Huu <buihuudong19@gmail.com>
2022-05-19 10:05:39 +0700

    commit

--- PRN_SE1629_PRODUCT_MANAGER/Program.cs
+++ PRN_SE1629_PRODUCT_MANAGER/Program.cs
@@ -1,10 +1,1 @@
 ﻿namespace Prn.Se1629;
-public interface IProduct
-{
-    public abstract void Add(Product p);
-    public abstract bool Update(Product p);
-    public abstract bool Delete(Product p);
-    public abstract Product GetProduct(int productID);
-
-    void Display();
-}
