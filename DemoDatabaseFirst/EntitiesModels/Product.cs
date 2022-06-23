a869581ab4db0a79de9afbf89ef5c324f62f4dd5
Dongbh <Dongbh@LAPTHINKPAD>
2022-06-23 10:34:34 +0700

    commit

---
+++ DemoDatabaseFirst/EntitiesModels/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DemoDatabaseFirst.EntitiesModels
+{
+   
+    public partial class Product
+    {
+        public Product()
+        {
+            OrderDetails = new HashSet<OrderDetail>();
+        }
+    
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+        public int? SupplierId { get; set; }
+        public int? CategoryId { get; set; }
+        public string? QuantityPerUnit { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public short? UnitsInStock { get; set; }
+        public short? UnitsOnOrder { get; set; }
+        public short? ReorderLevel { get; set; }
+        public bool Discontinued { get; set; }
+
+        public virtual Category? Category { get; set; }
+        public virtual Supplier? Supplier { get; set; }
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
---
+++ DemoDatabaseFirst/Program.cs
@@ -0,0 +1,31 @@
+﻿using DemoDatabaseFirst.EntitiesModels;
+using Microsoft.EntityFrameworkCore;
+//1. initial object DBContext
+using (FptEduStoreContext db = new())
+{
+    IQueryable<Category> categories = db.Categories;//.Include(c => c.Products);
+    foreach (var c in categories)
+    {
+        Console.WriteLine($"Category ID: {c.CategoryId}, Category Name: {c.CategoryName}, " +
+            $"Number of Products: {c.Products.Count}");
+        
+        var products = from pc in c.Products
+                       where pc.UnitPrice >= 18
+                       select pc;
+        
+        //or
+        /* var products2 = db.Products.Where(p => p.UnitPrice >= 18);*/
+
+
+
+        foreach (var p in products)
+        {
+            Console.WriteLine($"\t - Product ID: {p.ProductId}, Product Name: {p.ProductName}, " +
+                $"Unit Price: {p.UnitPrice}");
+        }
+    }
+
+
+}
+
+Console.ReadLine();
